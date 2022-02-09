2d8879848a57151a63c034716875c9d6fd289f31
Sharnow <sharnow007@gmail.com>
2022-02-09 20:52:34 +0600

    MID_LAB_TASK_04_PHP

---
+++ MID_LAB_TASK_04_PHP/4.php
@@ -0,0 +1,14 @@
+<?php
+
+$numbers = [220, 40, 80];
+
+$max = $numbers[0];
+
+for ($i = 1; $i < 3; $i++) {
+    if ($numbers[$i] > $max) {
+        $max = $numbers[$i];
+    }
+}
+
+echo "Maximum number: " . $max;
+?>
